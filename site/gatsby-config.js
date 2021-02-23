7e04f0276d4082af6a23772026a07a4ae2ef8c07
ibrahimuylas <ibrahim@uylas.net>
2021-02-23 20:39:29 +0000

    added google analytics code

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-XKVR3E676J"
+        trackingId: "259631694"
       },
     },
     {
