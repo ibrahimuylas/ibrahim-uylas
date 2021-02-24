b2b770278713157e7f2894134fb3068c16444dc6
ibrahimuylas <ibrahim@uylas.net>
2021-02-24 12:39:57 +0000

    added google analytics key

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "259631694"
+        trackingId: "G-F1E0LCDMPJ"
       },
     },
     {
