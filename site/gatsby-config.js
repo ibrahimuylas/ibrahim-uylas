250ebd9cc8d3aef5a8eebd013d0006f7ae4074e5
ibrahimuylas <ibrahim@uylas.net>
2021-02-24 12:42:06 +0000

    added google analytics key ua version

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-F1E0LCDMPJ"
+        trackingId: "UA-190518336-1"
       },
     },
     {
